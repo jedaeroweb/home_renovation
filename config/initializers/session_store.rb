1c160b8e89d8cf7c6f099583433fcd41d638cee8
sleepinglion(Mac27) <toughjjh@gmail.com>
2024-12-19 12:41:19 +0900

    modified:   config/initializers/session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
-session_url = "#{ENV.fetch('REDIS_CACHE_URL', 'redis://127.0.0.1:6379')}/0/session"
+session_url = "#{ENV.fetch('REDIS_SESSION_URL', 'redis://127.0.0.1:6379/1')}/session"
 secure = Rails.env.production?
 key = Rails.env.production? ? "_app_session" : "_app_session_#{Rails.env}"
 domain = ENV.fetch("DOMAIN_NAME", "localhost")
